adfc04ecac1dc8320fd5847abf6d616ee638b296
Yuri Gelfand <smartthings@shlitz.com>
2016-10-26 14:02:01 -0400

    bulk commit

---
+++ devicetypes/dsc/dsc-command-center.src/dsc-command-center.groovy
@@ -0,0 +1,101 @@
+/**
+ *  DSC Command Center
+ *
+ *  Author: David Cauthron
+ *  Also Attributed:  JTT-AE <aesystems@gmail.com>
+ *                    Rob Fisher <robfish@att.net>
+ *					  Carlos Santiago <carloss66@gmail.com> 
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "DSC Command Center", author: "me", namespace: "dsc") {
+		capability "Switch"
+		capability "polling"
+		
+		command "stayarm"
+		command "arm"
+		command "disarm"
+		}
+
+	// simulator metadata
+	simulator {
+	}
+
+	// UI tile definitions
+	tiles(scale: 2) {
+    
+
+		multiAttributeTile(name:"status", type: "generic", width: 6, height: 4){
+     	   tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
+				attributeState "disarm", label:'Disarmed - Ready', icon:"st.security.alarm.off", backgroundColor:"#79b821"
+				attributeState "arm", label:'Armed - Away', icon:"st.security.alarm.on", backgroundColor:"#800000"
+				attributeState "stayarm", label:'Armed - Stay', icon:"st.security.alarm.on", backgroundColor:"#008CC1"
+				attributeState "armed",     label: 'Armed',      backgroundColor: "#79b821", icon:"st.Home.home3"
+                attributeState "exitdelay", label: 'Exit Delay', backgroundColor: "#ff9900", icon:"st.Home.home3"
+      			attributeState "entrydelay",label: 'EntryDelay', backgroundColor: "#ff9900", icon:"st.Home.home3"
+ 			    attributeState "notready",  label: 'Open',       backgroundColor: "#ffcc00", icon:"st.Home.home2"
+  				attributeState "ready",     label: 'Ready',      backgroundColor: "#79b821", icon:"st.Home.home2"
+ 			    attributeState "alarm",     label: 'Alarm',      backgroundColor: "#ff0000", icon:"st.Home.home3"
+
+}
+		}		
+		standardTile("disarm", "capability.momentary", width: 2, height: 2, title: "Disarm", required: true, multiple: false){
+			state "disarm", label: 'Disarm', action: "disarm", icon: "st.Home.home4", backgroundColor: "#79b821"
+		}
+
+		standardTile("arm", "capability.momentary", width: 2, height: 2, title: "Arm", required: true, multiple: false){
+			state "arm", label: 'Arm', action: "arm", icon: "st.Home.home4", backgroundColor: "#800000"
+       }
+		standardTile("stayarm", "capability.momentary", width: 2, height: 2, title: "Armed Stay", required: true, multiple: false){
+			state "stayarm", label: 'Arm - Stay', action: "stayarm", icon: "st.Home.home4", backgroundColor: "#008CC1"
+       }
+
+		main (["status", "arm", "stayarm", "staybutton", "disarm"])	
+		details(["status", "arm", "stayarm", "staybutton", "disarm"])	
+
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'switch' attribute
+
+}
+
+def partition(String state, String partition) {
+    // state will be a valid state for the panel (ready, notready, armed, etc)
+    // partition will be a partition number, for most users this will always be 1
+
+    log.debug "Partition: ${state} for partition: ${partition}"
+    sendEvent (name: "switch", value: "${state}")
+}
+
+// handle commands
+def arm() {
+	log.debug "Executing 'arm'"
+    parent.switchUpdate("arm")
+    //sendEvent (name: "switch", value: "arm")
+}
+
+def stayarm() {
+	log.debug "Executing 'stayarm'"
+    parent.switchUpdate("stayarm")
+
+   // sendEvent (name: "switch", value: "stayarm")
+}
+
+def disarm() {
+	log.debug "Executing 'disarm'"
+    parent.switchUpdate("disarm")
+
+//sendEvent (name: "switch", value: "disarm")
+}
---
+++ devicetypes/dsc/dsc-zone.src/dsc-zone.groovy
@@ -0,0 +1,63 @@
+/*
+ *  DSC Zone Device
+ *
+ *  Author: Matt Martz <matt.martz@gmail.com>
+ *  Date: 2014-04-28
+ */
+
+// for the UI
+metadata {
+  definition (name: "DSC Zone", author: "me@me.com", namespace: "dsc" ) {
+    // Change or define capabilities here as needed
+    capability "Refresh"
+    capability "Contact Sensor"
+    capability "Polling"
+
+    // Add commands as needed
+    command "zone"
+  }
+
+  simulator {
+    // Nothing here, you could put some testing stuff here if you like
+  }
+
+  tiles {
+    // Main Row
+    standardTile("zone", "device.contact", width: 2, height: 2, canChangeBackground: true, canChangeIcon: true) {
+      state "open",   label: '${name}', icon: "st.contact.contact.open",   backgroundColor: "#ffa81e"
+      state "closed", label: '${name}', icon: "st.contact.contact.closed", backgroundColor: "#79b821"
+      state "alarm",  label: '${name}', icon: "st.contact.contact.open",   backgroundColor: "#ff0000"
+      state "bypass",  label: '${name}', icon: "st.contact.contact.open",   backgroundColor: "#c88ad1"
+    }
+    valueTile("lastEvent", "state.lastEvent", inactiveLabel: false, width: 2, height: 2) {
+            state "lastEvent", label:'${currentValue}%', unit:""
+        }
+    // This tile will be the tile that is displayed on the Hub page.
+    main "zone"
+
+    // These tiles will be displayed when clicked on the device, in the order listed here.
+    details(["zone"])
+  }
+}
+
+// handle commands
+def zone(String status) {
+  // state will be a valid state for a zone (open, closed)
+  // zone will be a number for the zone
+  //state.lastEvent = now()
+ // updateDataValue("lastEvent", now());
+ // log.debug "Zone: ${status}"
+  sendEvent (name: "contact", value: "${status}")
+}
+
+def poll() {
+  log.debug "Executing 'poll'"
+  // TODO: handle 'poll' command
+  // On poll what should we do? nothing for now..
+}
+
+def refresh() {
+  log.debug "Executing 'refresh' which is actually poll()"
+  poll()
+  // TODO: handle 'refresh' command
+}
